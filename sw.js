3ca36e8bb8f65e80015cb8a89b90ca2ea88bcea9
Pierovski <pieroczavaleta96@gmail.com>
2026-08-05 17:24:10 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pierovski-v6.1'; // Sube este número cada vez que modifiques tu código
+const CACHE_NAME = 'pierovski-v6.2'; // Sube este número cada vez que modifiques tu código
 
 const ASSETS = [
   './',
